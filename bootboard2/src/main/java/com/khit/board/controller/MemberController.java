2950965db63f91692100addbe146d0666a4df029
InkyDChoyee <choyee0421@naver.com>
2024-01-23 17:48:54 +0900

    Feat: member_crud

--- bootboard2/src/main/java/com/khit/board/controller/MemberController.java
+++ bootboard2/src/main/java/com/khit/board/controller/MemberController.java
@@ -1,10 +1,14 @@
 package com.khit.board.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.khit.board.entity.Member;
 import com.khit.board.service.MemberService;
@@ -48,5 +52,56 @@ public class MemberController {
 		return "redirect:/";
 	}
 	
+	// 회원 가입 페이지
+	@GetMapping("/member/join")
+	public String joinForm() {
+		return "/member/join";
+	}
 	
+	// 회원 가입 처리
+	@PostMapping("/member/join")
+	public String join(@ModelAttribute Member member) {
+		memberService.save(member);
+		return "redirect:/member/login";
+	}
+	
+	// 회원 목록
+	@GetMapping("/member/list")
+	public String getList(Model model) {
+		// DB에서 꺼내서 MemberDTO로 반환
+		List<Member> memberList = memberService.findAll();
+		model.addAttribute("memberList", memberList);
+		return "/member/list";   // list.html
+	}
+	
+	// 회원 상세 보기
+	@GetMapping("member/{id}")
+	public String getMember(@PathVariable("id") Integer id, Model model) {
+		Member member = memberService.findById(id);
+		model.addAttribute("member", member);
+		return "/member/detail";
+	} 
+	
+	// 회원 삭제
+	@GetMapping("member/delete/{id}")
+	public String deleteMember(@PathVariable("id") Integer id) {
+		memberService.deleteById(id);
+		return "redirect:/member/list";
+	}
+	
+	// 회원 수정
+	@GetMapping("/member/update/{id}")
+	public String updateForm(@PathVariable("id") Integer id, Model model) {
+		
+		Member member = memberService.findById(id);
+		model.addAttribute("member", member);
+		return "/member/update";
+	}
+	
+	// 회원 수정 처리
+	@PostMapping("/member/update")
+	public String update(@ModelAttribute Member member) {
+		memberService.update(member);
+		return "redirect:/member/" + member.getId();
+	}
 }
--- bootboard2/src/main/java/com/khit/board/service/MemberService.java
+++ bootboard2/src/main/java/com/khit/board/service/MemberService.java
@@ -1,10 +1,14 @@
 package com.khit.board.service;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.khit.board.entity.Member;
+import com.khit.board.entity.Role;
+import com.khit.board.exception.BootBoardException;
 import com.khit.board.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -13,6 +17,8 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class MemberService {
 	private final MemberRepository memberRepository;
+	
+	private final PasswordEncoder pwEncoder;
 
 	public Member login(Member member) {
 		// db에서 아이디 조회
@@ -27,4 +33,33 @@ public class MemberService {
 		
 	}
 
+	public void save(Member member) {
+		// 1. 비밀번호 암호화
+		// 2. 권한 설정
+		String encPw = pwEncoder.encode(member.getPassword());
+		member.setPassword(encPw);
+		member.setRole(Role.MEMBER);
+		memberRepository.save(member);
+	}
+
+	public List<Member> findAll() {
+		return memberRepository.findAll();
+	}
+
+	public Member findById(Integer id) {
+		Optional<Member> findMember = memberRepository.findById(id);
+		if(findMember.isPresent()) {
+			return findMember.get();
+		}else {
+			throw new BootBoardException("페이지를 찾을 수 없습니다");
+		}
+	}
+
+	public void deleteById(Integer id) {
+		memberRepository.deleteById(id);
+	}
+
+	public void update(Member member) {
+		memberRepository.save(member);
+	}
 }
